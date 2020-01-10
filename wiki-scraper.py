c93249bc26ea6c8002334a7271953ef420a42527
violenil <retrospect@protonmail.com>
2020-01-10 17:37:02 +0100

    starting to scrape movie name and directed by

--- wiki-scraper.py
+++ wiki-scraper.py
@@ -8,12 +8,16 @@ def scrapeWikiMovie(url):
     res = requests.get(url)  # retrieces the page
     res.raise_for_status()   # checks for any errors
     wikiSoup = bs4.BeautifulSoup(res.text, "html.parser") # res.text is all text from page, html.parser helps to structure the text into html format
-    wikiContents = []
     infoTable = wikiSoup.find(class_="infobox vevent")
-    directorTags= infoTable.find_all('')
-    #print(directorTag)
-    #directors = directorTag.find('td')
-    #d = directors.getText()
-    #print(d)
+    
+    directedBy = ""
+    movieName = infoTable.tr.string
+
+    all_tr = infoTable.find_all('tr')
+    for tr in all_tr:
+        if tr.th != None:
+            if tr.th.string == "Directed by":
+                directedBy = tr.th.next_sibling.string
+    print("Movie: " + movieName + "\n" + "Directed by: " + directedBy)
 
 scrapeWikiMovie("https://en.wikipedia.org/wiki/Once_Upon_a_Time_in_Hollywood")
